e6fafce0960cd21fa770588f4070312786400640
Ivan Domenzain <ivand@chalmers.se>
2022-06-21 17:59:24 +0200

    fix: correct numbering of sections in main script

--- code/find_gene_targets.m
+++ code/find_gene_targets.m
@@ -6,11 +6,11 @@ git clone --quiet --depth=1 https://github.com/SysBioChalmers/GECKO.git
 cd ..
 %ecModels catalogue
 git clone --quiet --depth=1 https://github.com/SysBioChalmers/ecModels.git
-%load wild-type ecModel
+%% 2.- load wild-type ecModel
 load(['ecModels/ecYeastGEM/model/ecYeastGEM_batch.mat'])
 ecModel = ecModel_batch;
 clc
-%% 2.- Set case-specific variables
+%% 3.- Set case-specific variables
 %Indicate carbon source uptake reaction name
 c_source   = 'D-glucose exchange (reversible)';
 %Indicate carbon source mol. weight in grams/mmol
@@ -24,7 +24,7 @@ product_name = '2-phenylethanol';
 %results folder name
 results_folder = '../../results/2_phenylethanol_targets';
 mkdir(results_folder)
-%% 3.- Generate production model
+%% 4.- Generate production model
 %Check presence of product in ecModel.metNames
 prod_pos = find(strcmpi(ecModel.metNames,product_name));
 %get extracellular compartment Index
@@ -62,10 +62,9 @@ if ~isempty(prod_pos)
 else
     error('Your provided product is not part of the used ecModel metabolites list')
 end 
-%% 4.- Check compatibility with the method
-cd method
-prod_ecModel = check_enzyme_fields(prod_ecModel);
+
 %% 5.-Set constraints
+cd method
 %Set media conditions for batch growth
 const_ecModel = changeMedia_batch(prod_ecModel,c_source,'Min'); %model-specific script
 CS_index      = find(strcmpi(const_ecModel.rxnNames,c_source));
@@ -85,14 +84,16 @@ else
     error('Your ecModel is not suited for production, please revise your production pathway connectivity')
 end
 disp(' ')
-%% 5.- Get max. biomass yield
+%% 6.- Get max. biomass yield
 WT_yield  = solution.x(growthPos)/(solution.x(CS_index)*CS_MW);
 disp(['* The maximum biomass yield is ' num2str(WT_yield) '[g biomass/g carbon source]']);
 %Obtain a suboptimal yield value to run ecFactory
 expYield = 0.49*WT_yield;
 disp('* The ecFactory method will scan flux distributions spanning from')
 disp(['a fixed biomass yield of: ' num2str(0.5*expYield) ' to: ' num2str(0.5*expYield) ' [g biomass/g carbon source]']);
-%% 6.- Run ecFactory method
+%% 7.- Run ecFactory method
+% Check compatibility with the method
+const_ecModel = check_enzyme_fields(const_ecModel);
 try
     [optStrain,candidates,step] = ecFactory(const_ecModel,target_rxn,const_ecModel.rxns(CS_index),expYield,CS_MW,results_folder);
     candidates.FCC = zeros(height(candidates),1);
